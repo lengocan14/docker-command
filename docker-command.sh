3e21cefb00f491f74105695208366f9cda48ca39
lengocan14 <lengocan14@gmail.com>
2022-04-19 20:11:05 +0900

    Update docker-command.sh

--- docker-command.sh
+++ docker-command.sh
@@ -8,3 +8,15 @@ docker run -d -p {outside port}:{docker port} {image name}:{tag}
 # -p for run background
 # ex
 docker run -d -p 80:8080 docker-example:v1
+
+# show all docker images
+docker images
+
+# show all docker containers
+docker ps -a
+
+# run into docker container
+# after run docker ps, we can get container id, use that id for this command
+docker exec -it {container id} bash
+# ex
+docker exec -it b2abecf12028 bash
